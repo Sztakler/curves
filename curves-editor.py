ad63888bdc895653d942175f8808bf8c635d0ba3
Krystian Jasionek <krystian.jasionek@gmail.com>
2023-04-17 19:36:55 +0200

    Added parametric equation input

--- curves-editor.py
+++ curves-editor.py
@@ -1,13 +1,46 @@
 import pygame
+import thorpy
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+pygame.display.flip()
 
 points = []
 
+
+slider = thorpy.SliderX(100, (12, 35), "My Slider")
+button = thorpy.make_button("Quit", func=thorpy.functions.quit_func)
+box = thorpy.Box(elements=[slider,button])
+#we regroup all elements on a menu, even if we do not launch the menu
+menu = thorpy.Menu(box)
+#important : set the screen as surface for all elements
+for element in menu.get_population():
+    element.surface = screen
+#use the elements normally...
+box.set_topleft((100,100))
+box.blit()
+box.update()
+
+
 while running:
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    
+    # Rendering game
+    for point in points:
+        pygame.draw.circle(surface = screen, color = "green", center = point, radius = 2.0)
+
+    if len(points) > 1:
+        pygame.draw.aalines(surface = screen, color = "yellow", closed=True, points=points)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60) # limits FPS to 60
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your windowk 
     for event in pygame.event.get():
@@ -21,19 +54,8 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             left_mouse_button_lock = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+        menu.react(event) #the menu automatically integrate your elements
 
-    # Rendering game
-    for point in points:
-        pygame.draw.circle(surface = screen, color = "green", center = point, radius = 2.0)
-
-    if len(points) > 1:
-        pygame.draw.aalines(surface = screen, color = "yellow", closed=True, points=points)
-    
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60) # limits FPS to 60
+  
 
 pygame.quit()
